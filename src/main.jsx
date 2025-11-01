f0bade0cdd22472b270526206df6b979d690f7a1
martorelljavier-prog <martorell.javier@gmail.com>
2025-11-01 18:10:54 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,9 @@ import App from './App.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
+// Registrar el Service Worker para que la app sea instalable y funcione offline
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
