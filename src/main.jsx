1310395ebdfd084af5351bcc736c8b20a709d515
martorelljavier-prog <martorell.javier@gmail.com>
2026-03-14 18:15:39 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -9,7 +8,6 @@ createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-// 👇 REGISTRO DEL SERVICE WORKER (necesario para instalación/offline)
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js");
